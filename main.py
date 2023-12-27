d96a603ef21cb09472fc18e27bf078920a9f99bb
Tochukwu Odoh <cashearncrypto@gmail.com>
2023-12-27 07:35:40 -0800

    working on databases

--- main.py
+++ main.py
@@ -18,3 +18,28 @@ if __name__ == '__main__':
 #  set FLASK_APP=microblog.py
 # flask db init
 # flask db migrate -m "users table
+# flask db upgrade
+# A foreign key, because it references a primary key of another table.
+# flask db migrate -m "posts table"
+# flask db upgrade
+# >>> from app import app, db
+# >>> from app.models import User, Post
+# >>> import sqlalchemy as sa
+# >>> app.app_context().push()
+# >>> u = User(username='john', email='john@example.com')
+# >>> db.session.add(u)
+# >>> db.session.commit()
+# >>> query = sa.select(User)
+# >>> users = db.session.scalars(query).all()
+# >>> users
+# [<User john>, <User susan>]
+#  u = db.session.get(User, 1)
+# >>> p = Post(body='my first post!', author=u)
+# >>> db.session.add(p)
+# >>> db.session.commit()
+# (venv) $ flask db downgrade base
+
+# (venv) $ >>> query = sa.select(User).order_by(User.username.desc())
+# >>> db.session.scalars(query).all()
+# [<User susan>, <User john>]
+# (venv) $ flask shell
